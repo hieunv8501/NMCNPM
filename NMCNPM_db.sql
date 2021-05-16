57c12a3f71109cfd62a104a1c68e6dc939c3a81c
= <=>
2021-05-16 22:36:56 +0700

    Tinh commit

--- NMCNPM_db.sql
+++ NMCNPM_db.sql
@@ -1,6 +1,4 @@
-﻿
-
-create database QLDKMH
+﻿create database QLDKMH
 go
 use QLDKMH
 go
@@ -57,6 +55,41 @@ create table NGANH
 	MaNganh char(4) primary key,
 	TenNganh nvarchar(40),
 	MaKhoa char(4)
+)
+
+--table LOAIMONHOC
+CREATE TABLE LOAIMONHOC
+(
+	MaLoai		char(4) PRIMARY KEY,
+	TenLoai		nvarchar(10),
+	TỉLeChia	int,
+)
+
+--table MONHOC
+CREATE TABLE MONHOC
+(
+	MaMonHoc	char(5) PRIMARY KEY,
+	TenMonHoc	nvarchar(50),
+	MaLoai		char(4) CONSTRAINT FK_MH_LMH FOREIGN KEY REFERENCES LOAIMONHOC(MaLoai),
+	SoTiet		int,
+	SoTinChi	int,
+)
+
+--table HOCKY
+CREATE TABLE HOCKY
+(
+	MaHocKy		char(3) PRIMARY KEY,
+	TenHocKy	nvarchar(20)
+)
+
+--table CT_CHUONGTRINHHOC
+CREATE TABLE CT_CHUONGTRINHHOC	
+(
+	MaNganh		char(4) CONSTRAINT FK_CTCTH_NGANH FOREIGN KEY REFERENCES NGANH(MaNganh),
+	MaMonHoc	char(5) CONSTRAINT FK_CTCTH_MONHOC FOREIGN KEY REFERENCES MONHOC(MaMonHoc),
+	MaHocKy		char(3) CONSTRAINT FK_CTCTH_HOCKY FOREIGN KEY REFERENCES HOCKY(MaHocKy),
+	GhiChu		nvarchar(50),
+	PRIMARY KEY (MaNganh, MaMonHoc)
 )
 
 --table PHIEUTHU
