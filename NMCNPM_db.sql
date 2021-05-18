b71a8cfe9156eb9bf9ca38a51c090f35dc373637
HAU PHAMPHUC <phamphuchauthcsquangtrung@gmail.com>
2021-05-18 12:02:33 +0700

    Update 2.4.3 and 2.4.4 nha

--- NMCNPM_db.sql
+++ NMCNPM_db.sql
@@ -1,4 +1,4 @@
-﻿create database QLDKMH
+﻿--create database QLDKMH
 go
 use QLDKMH
 go
@@ -61,9 +61,9 @@ create table NGANH
 CREATE TABLE PHIEUTHU
 (
 	MaPhieuThu int primary key,
-	MaPhieuDKHP int not null, --References to DKHP(MaPhieuDKHP),
-	NgayLap smalldatetime,
-	SoTienThu money
+	MaPhieuDKHP int not null, --References to DKHP(SoPhieuDKHP),
+	NgayLap smalldatetime not null ,
+	SoTienThu money not null
 )
 
 --table HOCKY_NAMHOC
@@ -73,7 +73,7 @@ CREATE TABLE HOCKY_NAMHOC
 	HocKy int not null, --References to DKHP(HocKy),
 	NamNK1 int,
 	NamNK2 int,
-	HanDongHocPhi smalldatetime
+	HanDongHocPhi smalldatetime not null
 )
 
 --table DSSV_CHUAHOANTHANH_HP
@@ -81,8 +81,8 @@ CREATE TABLE DSSV_CHUAHOANTHANH_HP
 (
 	STT int,
 	MaHKNH int not null,
-	MaSV char(6) not null,
-	SoTienConLai money,
+	MaSV char(6) not null,--References SINHVIEN(MaSV)
+	SoTienConLai money not null,
 	primary key (MaHKNH, MaSV)
 )
 
@@ -99,4 +99,16 @@ alter table SINHVIEN add constraint FK_SV_HUYEN foreign key (MaHuyen) references
 alter table SINHVIEN add constraint FK_SV_NGANH foreign key (MaNganh) references NGANH(MaNganh)
 alter table SINHVIEN add constraint FK_SV_DOITUONG foreign key (MaDoiTuong) references DOITUONG(MaDoiTuong)
 alter table NGANH add constraint FK_NGANH_KHOA foreign key (MaKhoa) references KHOA(MaKhoa)
+--Table cho chuc nang phan quyen.
+--Table Chuc Nang
+CREATE TABLE CHUCNANG(MaChucNang VARCHAR(30) primary key ,TenChucNang NVARCHAR(50),TenManHinhDuocLoad CHAR(20))
+/*Dữ liệu của Table này do người xây dựng hệ thống nhập đầy đủ cho nó
+trước khi triển khai hệ thống cho người dùng cuối sử dụng.
+Dữ liệu của các Table còn lại đều có thể được xem/thêm/xoá/sửa bởi
+người quản trị hệ thống (người có quyền cao nhất)*/
+CREATE TABLE NHOMNGUOIDUNG(MaNhom CHAR(10) primary key,TenNhom NVARCHAR(50))
+CREATE TABLE PHANQUYEN(MaNhom CHAR(10) references NHOMNGUOIDUNG(MaNhom),MaChucNang VARCHAR(30) REFERENCES CHUCNANG(MaChucNang))
+CREATE TABLE NGUOIDUNG(TenDangNhap VARCHAR(50),MatKhau VARCHAR(20),MaNhom CHAR(10)REFERENCES NHOMNGUOIDUNG(MaNhom))
+
+
 
