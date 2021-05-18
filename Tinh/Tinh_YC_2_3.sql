169ff4b25ab57f3ba81c87e3607677170ce6dd59
= <=>
2021-05-18 10:05:11 +0700

    Tinh YC_2_3

---
+++ Tinh/Tinh_YC_2_3.sql
@@ -0,0 +1,37 @@
+﻿
+
+--table LOAIMONHOC
+CREATE TABLE LOAIMONHOC
+(
+	MaLoaiMon	char(4) PRIMARY KEY,
+	TenLoai		nvarchar(10),
+	TỉLeChia	int,
+	SoTienTC	smallmoney,
+)
+
+--table MONHOC
+CREATE TABLE MONHOC
+(
+	MaMonHoc	char(5) PRIMARY KEY,
+	TenMonHoc	nvarchar(50),
+	MaLoaiMon	char(4) CONSTRAINT FK_MH_LMH FOREIGN KEY REFERENCES LOAIMONHOC(MaLoai),
+	SoTiet		int,
+	SoTinChi	int,
+)
+
+--table HOCKY
+CREATE TABLE HOCKY
+(
+	MaHocKy		char(3) PRIMARY KEY,
+	TenHocKy	nvarchar(20)
+)
+
+--table CHUONGTRINHHOC
+CREATE TABLE CT_CHUONGTRINHHOC	
+(
+	MaNganh		char(4) CONSTRAINT FK_CTCTH_NGANH FOREIGN KEY REFERENCES NGANH(MaNganh),
+	MaMonHoc	char(5) CONSTRAINT FK_CTCTH_MONHOC FOREIGN KEY REFERENCES MONHOC(MaMonHoc),
+	MaHocKy		char(3) CONSTRAINT FK_CTCTH_HOCKY FOREIGN KEY REFERENCES HOCKY(MaHocKy),
+	GhiChu		nvarchar(50),
+	PRIMARY KEY (MaNganh, MaMonHoc)
+)
