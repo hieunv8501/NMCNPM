da58f2f5fab29e36e46a6f04e2f118568f3fc9f9
HieuNV <hieunv8501@gmail.com>
2021-05-23 14:37:53 +0700

    update all

--- NMCNPM_db.sql
+++ NMCNPM_db.sql
@@ -206,5 +206,5 @@ alter table DOITUONG add constraint CHECK_TILE check (TiLeGiamHocPhi >= 0)
 alter table LOAIMONHOC add constraint CHECK_HESOCHIA check (HeSoChia > 0)
 alter table LOAIMONHOC add constraint CHECK_SOTIENMOTINCHI check (SoTienMotTinChi > 0)
 alter table MONHOC add constraint CHECK_SOTIET check (SoTiet > 0)
-alter table MONHOC add constraint CHECK_SOTC check (SoTinChi > 0)
+alter table MONHOC add constraint CHECK_SOTINCHI check (SoTinChi > 0)
 
