2b96cfdb39409684fa5a4845fc0754890369d5fc
HieuNV <hieunv8501@gmail.com>
2021-06-19 15:00:27 +0700

    .

--- Code VS/QLDKMH_CNPM/Areas/SV/Controllers/CHUONGTRINHHOCsController.cs
+++ Code VS/QLDKMH_CNPM/Areas/SV/Controllers/CHUONGTRINHHOCsController.cs
@@ -15,14 +15,26 @@ namespace QLDKMH_CNPM.Areas.SV.Controllers
         private CNPM_DBContext db = new CNPM_DBContext();
 
         // GET: SV/CHUONGTRINHHOCs/Index
-        public ActionResult Index()
+        public ActionResult Index(string id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ViewData["TenDangNhap"] = id;
+            //string id_nganh = db.SINHVIENs.Find(id).MaNganh.ToString();
+            //var cHUONGTRINHHOC = db.CHUONGTRINHHOCs.Include(c => c.MONHOC).Include(c => c.NGANH).Where(c => c.MaNganh == id_nganh).OrderBy(c => c.HocKy);
             return View(db.NGANHs.ToList());
         }
 
         // GET: SV/CHUONGTRINHHOCs/Details/5
-        public ActionResult Details(string id)
+        public ActionResult Details(string id, string id_2)
         {
+            ViewData["TenDangNhap"] = id_2;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             ViewBag.TenNganh = db.NGANHs.Find(id).TenNganh;
             ViewBag.MaNganh = db.NGANHs.Find(id).MaNganh;
 
--- Code VS/QLDKMH_CNPM/Areas/SV/Controllers/DS_MONHOC_MOController.cs
+++ Code VS/QLDKMH_CNPM/Areas/SV/Controllers/DS_MONHOC_MOController.cs
@@ -1,9 +1,12 @@
-﻿using QLDKMH_CNPM.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using QLDKMH_CNPM.Models;
 
 namespace QLDKMH_CNPM.Areas.SV.Controllers
 {
@@ -11,22 +14,33 @@ namespace QLDKMH_CNPM.Areas.SV.Controllers
     {
 
         private CNPM_DBContext db = new CNPM_DBContext();
-        public ActionResult ListHK()
+        public ActionResult ListHK(string id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ViewData["TenDangNhap"] = id;
             return View(db.HKNHs.ToList());
         }
         // GET: SV/DS_MONHOC_MO
-        public ActionResult Index(int id)
+        public ActionResult Index(int id, string id_2)
         {
+            ViewData["TenDangNhap"] = id_2;
             ViewBag.MaHKNH = db.HKNHs.Find(id).MaHKNH;
             ViewBag.HocKy = db.HKNHs.Find(id).HocKy;
             ViewBag.Nam1 = db.HKNHs.Find(id).Nam1;
             ViewBag.Nam2 = db.HKNHs.Find(id).Nam2;
             return View(db.DS_MONHOC_MO.Where(x => x.MaHKNH == id).ToList());
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 345e069568913a8d11805816bbe0f1793a934c93
