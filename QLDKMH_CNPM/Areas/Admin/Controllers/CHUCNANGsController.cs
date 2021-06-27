05033eef1d1710cffde954bb2966b2304dd5b361
HieuNV <hieunv8501@gmail.com>
2021-06-27 19:30:09 +0700

    update code

---
+++ QLDKMH_CNPM/Areas/Admin/Controllers/CHUCNANGsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLDKMH_CNPM.Models;
+
+namespace QLDKMH_CNPM.Areas.Admin.Controllers
+{
+    public class CHUCNANGsController : Controller
+    {
+        private CNPM_DBContext db = new CNPM_DBContext();
+
+        // GET: Admin/CHUCNANGs
+        public ActionResult Index()
+        {
+            return View(db.CHUCNANGs.ToList());
+        }
+
+        // GET: Admin/CHUCNANGs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CHUCNANG cHUCNANG = db.CHUCNANGs.Find(id);
+            if (cHUCNANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cHUCNANG);
+        }
+
+        // GET: Admin/CHUCNANGs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/CHUCNANGs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaChucNang,TenChucNang,TenManHinhDuocLoad")] CHUCNANG cHUCNANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CHUCNANGs.Add(cHUCNANG);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cHUCNANG);
+        }
+
+        // GET: Admin/CHUCNANGs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CHUCNANG cHUCNANG = db.CHUCNANGs.Find(id);
+            if (cHUCNANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cHUCNANG);
+        }
+
+        // POST: Admin/CHUCNANGs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaChucNang,TenChucNang,TenManHinhDuocLoad")] CHUCNANG cHUCNANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cHUCNANG).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cHUCNANG);
+        }
+
+        // GET: Admin/CHUCNANGs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CHUCNANG cHUCNANG = db.CHUCNANGs.Find(id);
+            if (cHUCNANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cHUCNANG);
+        }
+
+        // POST: Admin/CHUCNANGs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CHUCNANG cHUCNANG = db.CHUCNANGs.Find(id);
+            db.CHUCNANGs.Remove(cHUCNANG);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QLDKMH_CNPM/Areas/SV/Controllers/SVController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLDKMH_CNPM.Models;
+
+namespace QLDKMH_CNPM.Areas.SV.Controllers
+{
+    public class SVController : Controller
+    {
+        // GET: SV/SV
+        private CNPM_DBContext db = new CNPM_DBContext();
+
+        // Module load trang chủ của sinh viên khi có mã sinh viên, load trả về view data của sinh viên đó
+        public ActionResult SVHome(string TenDangNhap)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.FirstOrDefault(sv => sv.MaSV == TenDangNhap);
+
+            ViewData["TenDangNhap"] = sINHVIEN.MaSV;
+            return View();
+        }
+
+        //Module0 load chức năng xem giao diện sự kiện của sinh viên khi có mã sinh viên, load trả về view data của sinh viên đó
+        public ActionResult SVmodule0(string TenDangNhap)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.FirstOrDefault(sv => sv.MaSV == TenDangNhap);
+
+            ViewData["TenDangNhap"] = sINHVIEN.MaSV;
+            return View();
+        }
+        //Module0 load chức năng xem giao diện thông báo của sinh viên khi có mã sinh viên, load trả về view data của sinh viên đó
+        public ActionResult SVmodule1(string TenDangNhap)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.FirstOrDefault(sv => sv.MaSV == TenDangNhap);
+
+            ViewData["TenDangNhap"] = sINHVIEN.MaSV;
+            return View();
+        }
+        //Module0 load chức năng xem giao diện kế hoạch năm của sinh viên khi có mã sinh viên, load trả về view data của sinh viên đó
+        public ActionResult SVmodule2(string TenDangNhap)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.FirstOrDefault(sv => sv.MaSV == TenDangNhap);
+
+            ViewData["TenDangNhap"] = sINHVIEN.MaSV;
+            return View();
+        }
+        //Module0 load chức năng xem giao diện quy định - hướng dẫn của sinh viên khi có mã sinh viên, load trả về view data của sinh viên đó
+        public ActionResult SVmodule3(string TenDangNhap)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.FirstOrDefault(sv => sv.MaSV == TenDangNhap);
+
+            ViewData["TenDangNhap"] = sINHVIEN.MaSV;
+            return View();
+        }
+    }
+}
